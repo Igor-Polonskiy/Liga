fbbb7c4daeebca8331ff1b019bc6ea61bbd2fcfb
Igor Polonskiy-Buslaev <barlogfire@ya.ru>
2019-10-31 23:04:22 +0300

    nav

--- html/js/main.js
+++ html/js/main.js
@@ -1,11 +1,30 @@
 jQuery(function($){
 
-$('.nav_collapse').click(function(){
-  $(".nav_down").slideToggle(300);
+$('.nav_collapse_1').click(function(){
+  $(".nav_down_1").slideToggle(300);
+  $('.nav_down_2').slideUp(300);
+  $('.nav_down_3').slideUp(300);
+  
+  
+  console.log("yes");
 }); 
-$('header').focusout(function(){
-  setTimeout(function() {
+$('.nav_collapse_2').click(function(){
+  $(".nav_down_2").slideToggle(300);
+   $('.nav_down_1').slideUp(300);
+  $('.nav_down_3').slideUp(300);
+
+  console.log("yes");
+}); 
+$('.nav_collapse_3').click(function(){
+  $(".nav_down_3").slideToggle(300);
+   $('.nav_down_2').slideUp(300);
+  $('.nav_down_1').slideUp(300);
+  console.log("yes");
+}); 
+$('.nav_down').focusout(function(){
+  
+  	console.log("nen")
     $('.nav_down').slideUp(300);
-  }, 150);
+  
 })
 })
